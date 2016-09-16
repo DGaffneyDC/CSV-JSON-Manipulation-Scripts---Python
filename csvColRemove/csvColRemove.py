331d2f1636c5cb8ec5049896aba8a56f5494220d
DylanGaffney <dgaffney1@gmail.com>
2016-09-16 10:45:34 -0400

    csvColRemove.py functional

--- csvColRemove/csvColRemove.py
+++ csvColRemove/csvColRemove.py
@@ -1,12 +1,25 @@
 import csv
+import itertools
 
-def csvColRemove( inFile, outFile ):
+def csvColRemove( inFile, outFile, remove ):
+	index = remove
+	index.sort(reverse=True)
+	print "Removing columns in index position(s):  "
+	print index
 	with open(inFile,'rb') as csvFile:
-	    rdr= csv.reader( csvFile )
+	    rdr1, rdr2 = itertools.tee(csv.reader( csvFile ))
+	    numCols =len(next(rdr1))
+	    del rdr1
+	    columns = range(0, numCols)
+	    for i in index:
+			filtered = [c for c in columns if c not in index ]
 	    with open(outFile, 'wb') as result:
 	        wtr= csv.writer( result )
-	        for r in rdr:
-	            wtr.writerow( (r[1], r[2], r[3], r[4], r[7], r[8], r[9], r[10], r[11], r[50], r[51], r[52], r[53], r[54], r[55]) )
+	        for r in rdr2:
+	            wtr.writerow( [r[f] for f in filtered])
+	        
+	# print "File " + outFile + " exported with the following columns: "
+	# print filtered
 
 if __name__ == "__main__":
      import argparse
@@ -14,5 +27,6 @@ if __name__ == "__main__":
      parser = argparse.ArgumentParser()
      parser.add_argument('inFile', nargs=1, help="Choose the in file to use")
      parser.add_argument('outFile', nargs=1, help="Choose the out file to use")
+     parser.add_argument('-r', '--remove', nargs='*', type=int)
      args = parser.parse_args()
-     csvColRemove( args.inFile[0] , args.outFile[0] )
+     csvColRemove( args.inFile[0] , args.outFile[0] , args.remove);
